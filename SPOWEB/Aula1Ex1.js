81d3d26644d7091b68083a0d4e825e865bafee4e
LeviAlves01 <alveslevi457@gmail.com>
2025-04-15 16:16:33 -0300

    inicio do formulario

---
+++ SPOWEB/Aula1Ex1.js
@@ -0,0 +1,13 @@
+let x = 10;
+let y = 3;
+
+console.log('X = ' + x);
+console.log('Y = ' + y);
+console.log('-X = ' + (-x));
+console.log('X/Y = ' + (x/y));
+console.log('Resto de X por Y = ' + (x%y));
+console.log('Inteiro de X por Y = ' + (Math.floor(x/y)));
+console.log('X + 1 = ' + (++x)); // 11 = 10+1
+console.log('X = ' + x);
+console.log('X = ' + (x++)); // 10+1 = 11
+console.log('X = ' + x)
