649baca7dbf0f4b4adf4765d39ee32558cfb30b3
LeviAlves01 <alveslevi457@gmail.com>
2025-05-12 13:58:16 -0300

    exercicio 0505

--- ex0505.py
+++ ex0505.py
@@ -1,57 +1,90 @@
-print("Bem-vindo a Bistrô do Levi. \n Vamos iniciar o seu pedido.")
-
-class ItemCardapio:
+class ItemCardapio():
     def __init__(self, nome, preco, descricao):
         self.nome = nome
         self.preco = preco
         self.descricao = descricao
 
-    def exibir_detalhes(self):
-        print(f"{self.nome} - {self.descricao} - {self.preco}")
-
-    def get_preco(self):
-        return self.preco
-    
-    def get_nome(self):
+    def getnome(self):
         return self.nome
     
-    def get_descricao(self):
+    def setnome(self, novo_nome):
+        self.nome = novo_nome
+
+    def getdesc(self):
         return self.descricao
 
-class Cliente:
+    def setdesc(self, nova_desc):
+        self.descricao = nova_desc
+
+    def getpreco(self):
+        return self.preco
+
+    def setpreco(self, novo_preco):
+        self.preco = novo_preco
+    
+    def exibir_detalhes(self):
+        print(f'{self.getnome()} - {self.getpreco()} - {self.getdesc()}')
+
+class Cliente():
     def __init__(self, nomec, telefone):
         self.nomec = nomec
         self.telefone = telefone
-
-    def get_nomec(self):
-        return self.nomec
     
-    def get_telefone(self):
-        return self.telefone
-    
+    def exibir_detalhesC(self):
+        print(f'{self.nomec} - {self.telefone}')
 
-class Pedido(ItemCardapio):
-    def __init__(self, nome, preco, descricao):
-        super().__init__(nome, preco, descricao)
+class Pedido():
+    def __init__(self, cliente, status="Em Aberto."):
+        self.cliente = cliente
+        self.itens_pedido = [] 
+        self.status = status
 
-    def adicionar_item(self, ItemCardapio, ItensPedido, valortotal):
-        self.ItensPedido = [ItensPedido+ItemCardapio]
-        self.valortotal = valortotal + self.preco
-        
-        
-        for i in range(ItensPedido):
-            self.valortotal = valortotal + self.preco
-            i = 0
-            
-    
+    def adicionar_item(self, item):
+        self.itens_pedido.append(item)
+        print(f"Item {item.getnome()} adicionado ao pedido.")
 
-    def calcular_total(self, ItensPedido, total):
-        self.ItensPedido = ItensPedido
-        self.total = total
-        
-        
-cardapio_lanchonete = []
-xBurguer = ItemCardapio("XBurguer", "Hamburguer daora", 80.56)
+    def calcular_total(self):
+        total = 0
+        for item in self.itens_pedido:
+            total = total + item.getpreco()
+        return total
 
+    def exibir_resumo(self):
+        print("Informacoes do Cliente:")
+        print("")
+        self.cliente.exibir_detalhesC()
+        print("")
+        print("itens no pedido:")
+        print("------------------")
+        for item in self.itens_pedido:
+            print(f"Item: {item.getnome()} - Preco: R$ {item.getpreco()} - Descricao: {item.getdesc()}") 
+        print("------------------")
+        print(f"Valor total do pedido: R$ {self.calcular_total():.2f}")
+        print("------------------")
+        print(f"Status do pedido: {self.status}\n")
 
+    def finalizar_pedido(self):
+        self.status = "Finalizado"
 
+cliente1 = Cliente("lucs", "11111111111")
+item1 = ItemCardapio("X-Burguer", 17.51, "Pao, carne e queijo.")
+item2 = ItemCardapio("Batata Frita", 8.50, "Batatas fritas ao oleo.")
+item3 = ItemCardapio("Coca Cola Latinha", 999.50, "Refrigerante sabor noz de cola. Contem 350ml")
+
+pedido1 = Pedido(cliente1)
+
+pedido1.adicionar_item(item1)
+pedido1.adicionar_item(item2)
+pedido1.adicionar_item(item3)
+
+pedido1.exibir_resumo()
+
+finalizar = int(input("Pressione 1 para finalizar o pedido: "))
+print("------------------")
+if finalizar == 1:
+    pedido1.finalizar_pedido()
+    print("\nPedido Finalizado, imprimindo recibo:\n")
+    print("------------------")
+    print("Resumo do Pedido:")
+    print("")
+    pedido1.exibir_resumo()
