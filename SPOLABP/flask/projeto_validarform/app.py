aa7a2aa14844ec6af314b87e066fdd04351dc21c
LeviAlves01 <alveslevi457@gmail.com>
2025-09-12 02:03:28 -0300

    organização do repositório e adição de anotações de aulas

---
+++ SPOLABP/flask/projeto_validarform/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, flash, redirect, url_for
+
+
+app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = 'uma-chave-secreta-muito-segura'
+
+
+@app.route('/formulario', methods=['GET', 'POST'])
+def formulario():
+
+
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        email = request.form.get('email')
+
+
+        print(f"Dados recebidos do formulário: Nome={nome} Email={email}")
+
+
+        flash(f"Obrigado por se cadastrar, {nome}", "success")
+
+
+        return redirect(url_for('formulario'))
+   
+    return render_template('formulario.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- ola.py
+++
@@ -1,5 +0,0 @@
-class ItemCardapio:
-    def __init__(self, nome, preco, descricao):
-        self.preco = preco
-        self.nome = name
-        self.desc = desc
