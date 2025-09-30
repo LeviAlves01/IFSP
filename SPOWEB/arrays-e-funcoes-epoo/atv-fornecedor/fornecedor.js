a0a1e4f94cc2384970e8298da6f7c0026544698b
LeviAlves01 <alveslevi457@gmail.com>
2025-09-30 16:06:22 -0300

    exercicio fornecedor

---
+++ SPOWEB/arrays-e-funcoes-epoo/atv-fornecedor/fornecedor.js
@@ -0,0 +1,14 @@
+class Fornecedor{
+    constructor(nome, fone){
+        this.nome = nome;
+        this.fone = fone;
+    }
+
+    setNome(novoNome){
+        novoNome = this.nome;
+    }
+
+    getNome(novoNome){
+        return novoNome;
+    }
+}
